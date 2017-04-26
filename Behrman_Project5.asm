157af24fe5109472c8236b9fe7bfe7b36f0a89b8
Alex Behrman <alexbehrman89@gmail.com>
2017-04-25 21:09:39 -0400

    update comments

--- Behrman_Project5.asm
+++ Behrman_Project5.asm
@@ -1,15 +1,6 @@
 TITLE Programming Assingment #5    (Behrman_Project5.asm)
 
-; Author: Alexandra Behrman
-; Course / Project ID    CS271_400             Date: 2/28/16
-; Description: Write a program that performs the following tasks:
-	;1. Introduce the program.
-	;2. Get a user request in the range [min = 10 .. max = 200].
-	;3. Generate request random integers in the range [lo = 100 .. hi = 999], storing them in consecutive elements of an array.
-	;4. Display the list of integers before sorting, 10 numbers per line.
-	;5. Sort the list in descending order (i.e., largest first).
-	;6. Calculate and display the median value, rounded to the nearest integer.
-	;7. Display the sorted list, 10 numbers per line.
+;sort user-requested number of random integers
 
 INCLUDE Irvine32.inc
 
